132d9449e6b0efbb00906d2582216df081f9eee5
Ayan Rastogi <ayanrastogi04@gmail.com>
2026-03-22 21:46:29 +0530

    Fixed the failing frontend unit tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders emergency input header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Emergency Input/i);
+  expect(headerElement).toBeInTheDocument();
 });
